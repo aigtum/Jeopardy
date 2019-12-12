5de5ab468af296e0f82810317ea231a55dc2173e
junglebooboo <aigtum@gmail.com>
2019-12-12 14:36:36 +0100

    Showing list of questions sorted by topics

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -2,53 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import key from "weak-key";
 
-const buttonList = [];
-
-for (let i = 0; i <= 5; i++) {
-    let buttonRow = [];
-    for (let j = 0; j <= 5; j++) {
-        buttonRow.push(<td><button className={"card"} id={"t"+j+"-q"+(i+1)*200}>{(i+1)*200}</button></td>)
-    }
-    buttonList.push(<tr>{buttonRow}</tr>)
-}
 
 const Table = ({ topics, questions }) =>
   !topics.length ? (
     <p>Nothing to show</p>
   ) : (
-    <div className="row">
-      <h2 className="subtitle">
-        Showing <strong>{topics.length} items</strong>
-      </h2>
-
-        <table className="table is-striped">
-        <thead>
-
-                {topics.map(el => (
-            <th key={"topic"+el.id}>
-              {el["text"]}
-            </th>
+      <div>
+          <h2 className="subtitle">
+              Showing <strong>{topics.length} topics</strong>
+          </h2>
+          <h2>
+              Showing {questions.length} questions
+          </h2>
+          {topics.map(el => (
+              <h3 key={"topic"+el.id}>
+                  {el["text"]}
+                  {questions.map(question => (
+                      (el.id === question["topic"]) ? (
+                      <p key={"question" + question["id"]}>{question["text"]}: {question["topic"]}</p>
+                      ) : ("")
+                      ))}
+              </h3>
           ))}
+      </div>
 
-
-
-        </thead>
-        <tbody>
-        {buttonList.map(el => {
-            questions.map(q => {
-                let matchString = "t"+q["topic"]+"p"+q["points"];
-                console.log(matchString + "; " + el)
-                if (el.id == matchString) {
-                    return el
-                }
-
-            })
-        })}
-
-        </tbody>
-      </table>
-    </div>
   );
+
 
 Table.propTypes = {
     topics: PropTypes.array.isRequired,
